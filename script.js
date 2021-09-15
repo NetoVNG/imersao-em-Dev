09afe4893187ebcbb31cf52138bf113c0cfc4c3e
Neto Vasconcellos <85071601+NetoVNG@users.noreply.github.com>
2021-09-15 06:53:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//Coloquei JS sem qualquer motivo
+document.getElementById("titulo").innerHTML = "Projeto Imersão Dev <a href='https://imersao.dev/' class='imersao'>#imersaoDev</a> <a href='https://www.alura.com.br/' class='imersao'>#Alura</a>";
+document.getElementById("nome").innerHTML = "Neto Vasconcellos";
+document.getElementById("texto").innerHTML = "Abaixo o material de conclusão das aulas <a href='https://imersao.dev/' class='imersao'>#imersaoDev </a> <a href='https://www.alura.com.br/' class='imersao'>#Alura</a> abordando HTML5, CSS e JavaScript.";
